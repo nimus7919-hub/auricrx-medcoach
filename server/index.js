2c8d127fb5118a560b4885ed9030591d5e5e36a1
Your Name <your@email.com>
2025-09-23 22:24:56 -0600

    Fix server deployment - trigger redeploy with medication endpoints

--- server/index.js
+++ server/index.js
@@ -1186,7 +1186,7 @@ app.delete('/medication-contributions', async (req, res) => {
 });
 
 app.get('/', (_req, res) => {
-  res.send('AuricRx Medcoach API is running ✅');
+  res.send('AuricRx Medcoach API is running ✅ - Medication endpoints available!');
 });
 
 app.listen(port, () => {
